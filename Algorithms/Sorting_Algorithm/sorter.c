f5c9d463d1415eb76d51209743c96f17f04e922f
Andrew <124897070+AndrewCullacino@users.noreply.github.com>
2025-06-21 01:25:38 +0000

    Added all sorting algorithms so far. More practice is required.

--- Algorithms/Sorting_Algorithm/sorter.c
+++ Algorithms/Sorting_Algorithm/sorter.c
@@ -307,6 +307,7 @@ static void merge(Item items[], int lo, int mid, int hi) {
 
 ////////////////////////////////////////////////////////////////////////
 // Quicksort: divide and conquer: 分而治之
+// worst case O(n^2) Average: o(nlogn)
 
 /**
  * 1. select a pivot element from array
@@ -345,12 +346,26 @@ static void naiveQuickSort(Item items[], int lo, int hi)
 
 
 
+////////////////////////////////////
+// Quick sort: Median of three
+// reduce the chance of hitting worst case
 
+static void medianOfThreeQuickSort(Item items[], int lo, int hi)
+{
+    if (lo >= hi) return;
+    medianOfThree(items, lo, hi);
+    int pivotIndex = partition(items, lo, hi);
+    medianOfThreeQuickSort(items, lo, pivotIndex - 1);
+    medianOfThreeQuickSort(items, pivotIndex + 1, hi);
+}
 
-
-
-
-
+static void medianOfThree(Item a[], int lo, int hi)
+{
+    int mid = (lo + hi) / 2;
+    if (gt(a[mid], a[lo])) swap(a, mid, lo);
+    if (gt(a[lo],  a[hi])) swap(a, lo,  hi);
+    if (gt(a[mid], a[lo])) swap(a, mid, lo);
+}
 
 
 
