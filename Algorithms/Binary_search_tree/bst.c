cc66766acb15c6f241da888103cc2a20aedb6ee9
Andrew <124897070+AndrewCullacino@users.noreply.github.com>
2025-06-23 08:04:22 +0000

    Initialize the binary search algorithm tree.

---
+++ Algorithms/Binary_search_tree/bst.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "bst.h"
+
+struct node *newNode(int val);
+
+static void showBstNode(struct node *tree);
+
+static void doBstPrint(struct node *t, FILE *out);
+
+
+
+
+
+struct node *newNode(int val) {
+
+
+
+
+
+
+
+
+
+
+
+    
+}
