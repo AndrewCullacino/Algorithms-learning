19377a391dd55a68d8e7559f0023ac03919f7ca8
Jintian Wang <z5536837@ad.unsw.edu.au>
2025-06-25 22:17:14 +1000

    Added all functions from lecture.

--- Algorithms/Binary_search_tree/bst.c
+++ Algorithms/Binary_search_tree/bst.c
@@ -51,3 +51,68 @@ struct node *bstInsert(struct node *tree, int val) {
 
 	return tree;
 }
+
+bool bstSearch(struct node *tree, int val) {
+	
+    // Base case: If the tree is NULL, return false
+    if (tree == NULL) return false;
+    else if (tree->value > val) return bstSearch(tree->left, val);
+    else if (tree->value < val) return bstSearch(tree->right, val);
+    else return true;
+
+}
+
+/**
+ * Assumptions: the max of t1 < the min of t2
+ */
+struct node *bstJoin(struct node *t1, struct node *t2) {
+    if (t1 == NULL) return t2;
+    if (t2 == NULL) return t1;
+
+
+    // if t2 is only 1 node
+    if (t2->left == NULL) {
+        t2->left = t1;
+        return t2;
+    }
+
+    // Find the minimum of t2
+    struct node *parent = NULL;
+    struct node *min = t2;
+
+    // the bst is different from linked lists
+    // we cannot extract like min->next->left, so we have a seperate parent node to record
+    while (min->left != NULL) {
+        parent = min;
+        min = min->left;
+    }
+
+    parent->left = min->right;  // to put the min of t2 on the root, and avoid duplicate value of right
+    min->left = t1;
+    min->right = t2;
+
+
+	return min;
+}
+
+struct node *bstDelete(struct node *tree, int val) {
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
