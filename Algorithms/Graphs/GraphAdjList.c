f2247de666c8d97c5cdf4d55afb66ccb17f60d0f
Jintian Wang <z5536837@ad.unsw.edu.au>
2025-06-26 08:47:19 +1000

    Update the graph skeleton.

---
+++ Algorithms/Graphs/GraphAdjList.c
@@ -0,0 +1,10 @@
+// Implementation of the Graph ADT
+// Undirected, unweighted
+// Vertices are given by integers between 0 and V - 1
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Graph.h"
